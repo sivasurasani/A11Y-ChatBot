780f7ba0a6daa082744933f90e8542eba5bdd2b1
Surasani Siva Kumar Reddy <69511496+sivasurasani@users.noreply.github.com>
2024-03-06 10:50:52 -0600

    Interface

--- src/navbar.js
+++ src/navbar.js
@@ -1,16 +1,18 @@
 import React from "react";
+
 const Navbar = () => {
   return (
     <>
       <div className="navbar">
         <h1>A11Y Chatbot</h1>
-        <nav>
-          <a>Home</a>
-          <a>Features</a>
-          <a>Docs</a>
-        </nav>
+        {/* <nav>
+          <a href="/">Home</a>
+          <a href="/features">Features</a>
+          <a href="/docs">Docs</a>
+        </nav> */}
       </div>
     </>
   );
 };
+
 export default Navbar;
